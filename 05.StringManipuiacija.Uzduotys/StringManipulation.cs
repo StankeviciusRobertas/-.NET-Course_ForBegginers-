8faf4a673680de51c172fa7bcae32fd47baeaa3d
Robertas Stankevičius <stankevic.robertas@gmail.com>
2023-10-06 14:25:54 +0300

    Add project files.

---
+++ 05.StringManipuiacija.Uzduotys/StringManipulation.cs
@@ -0,0 +1,153 @@
+﻿namespace _05.StringManipuiacija.Uzduotys
+{
+    internal class StringManipulation
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskite teksta: ");
+            //string text = Console.ReadLine();
+            //string firstLetterUpper = text.Substring(0, 1).ToUpper() + text.Substring(1);
+            //Console.WriteLine(firstLetterUpper);
+
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskite teskta: ");
+            //string text = Console.ReadLine();
+            //char[] newText = text.ToCharArray();
+
+            //newText[2] = 'g';
+            //newText[4] = 'b';
+            //newText[6] = '*';
+            //newText[8] = 'x';
+            //newText[10] = 'w';
+
+            //string newString = new string(newText);
+            //Console.WriteLine(newString);
+
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskit 5 simboliu teksta: ");
+            //string text = Console.ReadLine();   
+
+            //if (text.Length == 5)
+            //{
+            //    Console.WriteLine("Kokiu skaiciumi noretumete uzkoduoti savo prensima? ");
+            //    string number = Console.ReadLine();
+            //    char[] numberChar = number.ToCharArray();
+            //    char[] textOfChars = text.ToCharArray();
+
+            //    textOfChars[0] += numberChar[0];
+            //    textOfChars[1] += numberChar[0];
+            //    textOfChars[2] += numberChar[0];
+            //    textOfChars[3] += numberChar[0];
+            //    textOfChars[4] += numberChar[0];
+
+            //    string codetText = new string(textOfChars);
+            //    Console.WriteLine(codetText);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("blogai ivestas teksto ilgis");
+            //}
+
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskite sakini: ");
+            //string text = Console.ReadLine();
+
+            //string changedA = text.Replace("a", "uo");
+            //Console.WriteLine(changedA);
+            //string changedI = text.Replace("i", "e");
+            //Console.WriteLine(changedI);
+
+            //string changedText = text
+            //.Replace("a", "uo")
+            //.Replace("i", "e");
+            //Console.WriteLine(changedText);
+
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskite teksta: ");
+            //string text = Console.ReadLine();
+
+            //Console.WriteLine("Iveskite zodi kuri noretumete pakeisti is teksto: ");
+            //string findWord = Console.ReadLine();
+
+            //Console.WriteLine("Iveskite zodi i kuri noretumete pakeisti ji tekste: ");
+            //string changeWrod = Console.ReadLine();
+
+            //string changedWord = text.Replace(findWord, changeWrod);
+
+            //Console.WriteLine($"Pakeistas sakinys: {changedWord}");
+
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskite kiek jums metu, paskaiciuosime kiek liko gyventi iki 90 metu: ");
+
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //int tillAge = 90;
+
+            //int leftAge = tillAge - age;
+            //int leftMonths = leftAge * 12;
+            //int leftWeeks = leftMonths * 4;
+            //int leftDays = leftWeeks * 7;       
+                        
+                        
+            //Console.WriteLine($"Jums liko gyventi dienu: {leftDays}" );
+            //Console.WriteLine($"Jums liko gyventi savaiciu: {leftWeeks}");
+            //Console.WriteLine($"Jums liko gyventi Menesiu: {leftMonths}");
+            //Console.WriteLine($"Jums liko gyventi Metu: {leftAge}");
+
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskite zodi: ");
+            //string word = Console.ReadLine();
+
+            //if (char.IsUpper(word[0]))
+            //{
+            //    string changedWord = word.ToUpper();
+            //    Console.WriteLine($"Pakeistas zodis: {changedWord}");
+            //}
+            //else if (char.IsLower(word[0]))
+            //{
+            //    string firstLetterUpper = word.Substring(0, 1).ToUpper() + word.Substring(1);
+            //    Console.WriteLine($"Pakeistas zodis: {firstLetterUpper}");
+            //}
+
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskite zodi: ");
+            //string word = Console.ReadLine();
+
+            //if (word.Contains("a"))
+            //{
+            //    Console.WriteLine($"zodis: {word} indeksas: {word.IndexOf("a")}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("simbolis 'a' nerastas");
+            //}
+
+            //Console.WriteLine("=========================================");
+
+            //Console.WriteLine("Iveskit zodi: ");
+            //string word = Console.ReadLine();
+
+            //if (word == "labas")
+            //{
+            //    char[] charWord = word.ToCharArray();
+            //    Array.Reverse(charWord);
+            //    string reversedWord = new string(charWord);
+            //    Console.WriteLine(reversedWord);
+            //}
+
+            Console.WriteLine("=========================================");
+
+
+
+            Console.WriteLine("=========================================");
+        }
+    }
+}
